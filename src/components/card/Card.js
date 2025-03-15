10843f09b2ff2985220476b83b80dd29923f5b84
dphuoc432000 <dphuoc432000@gmail.com>
2025-03-15 15:40:00 +0700

    first build

---
+++ src/components/card/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./card.css"
+
+const Card = () => {
+    return (
+        <div className="card">
+            <div className="jp-txt">
+                <div className="main-txt">あなた</div>
+                <div className="sub-txt"></div>
+            </div>
+            <div className="jp-transcription">anata</div>
+            <div className="jp-translate">Bạn</div>
+            <div className="note">Bạn</div>
+        </div>
+    );
+};
+
+export default Card;
